b03764b53cd1759eba1e8bee89306dd470abb9e0
suvarna411 <suvarna.tadikamalla@promantia.com>
2026-05-19 17:17:56 +0530

    Added backend locker booking validation

--- gym_management/gym_management/doctype/gym_locker_booking/gym_locker_booking.py
+++ gym_management/gym_management/doctype/gym_locker_booking/gym_locker_booking.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2026, suvarna and contributors
 # For license information, please see license.txt
 
-# import frappe
+
+import frappe
 from frappe.model.document import Document
 
 
 class GymLockerBooking(Document):
-	pass
+
+    def validate(self):
+
+        existing_booking = frappe.db.exists(
+            "Gym Locker Booking",
+            {
+                "locker": self.locker,
+                "name": ["!=", self.name]
+            }
+        )
+
+        if existing_booking:
+            frappe.throw("Locker is already booked")
