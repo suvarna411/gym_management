067aad0ee6e47bc738857e6ebad409a4d2ef4dd8
suvarna411 <suvarna.tadikamalla@promantia.com>
2026-05-19 17:45:01 +0530

    debugged test cases

--- gym_management/gym_management/doctype/gym_membership/test_gym_membership.py
+++ gym_management/gym_management/doctype/gym_membership/test_gym_membership.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2026, suvarna and Contributors
 # See license.txt
-# added a comment 
+
 import frappe
 
 from frappe.tests.utils import FrappeTestCase
@@ -11,17 +11,35 @@ class TestGymMembership(FrappeTestCase):
 
     def test_remaining_days_calculation(self):
 
+        # Create Gym Member
+        member = frappe.get_doc({
+                   "doctype": "Gym Member",
+                   "name1": frappe.generate_hash(length=6),
+                   "member_id": "MEM-001"
+        }).insert()
+
+        # Create Membership Plan
+        plan = frappe.get_doc({
+        "doctype": "Gym Membership Plan",
+        "plan_name": frappe.generate_hash(length=6),
+        "duration_months": 3,
+        "price": 5000
+        }).insert()
+
+        # Create Membership
         membership = frappe.get_doc({
 
             "doctype": "Gym Membership",
 
-            "gym_member": "Tomar",
+            "gym_member": member.name,
 
-            "membership_plan": "Gold Plan",
+            "membership_plan": plan.name,
 
             "start_date": nowdate(),
 
-            "end_date": add_days(nowdate(), 10)
+            "end_date": add_days(nowdate(), 10),
+
+            "status": "Active"
         })
 
         membership.insert()
